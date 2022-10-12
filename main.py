dad6c7ef52cfb7e69f5d3716d1f4c634910d81b6
AndBoyS <andboyxd@gmail.com>
2022-10-12 20:12:23 +0300

    better button gui

--- main.py
+++ main.py
@@ -1,39 +1,19 @@
 import json
 import random
 
-from kivy.metrics import dp
 from kivymd.app import MDApp
 from kivymd.uix.button import MDRectangleFlatButton, MDIconButton, MDFloatingActionButton
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
 
 
-class BigAssButton(MDFloatingActionButton):
-    """
-    Опций в родительской кнопке на размер всего три
-
-    Эта штука позволит увеличить кнопки с типом large еще больше
-    """
-
-    # Насколько мы увеличиваем размер кнопки по сравнению с обычной версией large
-    multiplier = 1.3
-
-    def set__radius(self, *args) -> None:
-        super().set__radius(*args)
-        if self.type == "large" and self.theme_cls.material_style != "M2":
-            self._radius = dp(int(28*self.multiplier))
-
-    def set_size(self, *args) -> None:
-        super().set_size(*args)
-        if self.type == "large" and self.theme_cls.material_style != "M2":
-            raw_size = int(96*self.multiplier)
-            self.size = dp(raw_size), dp(raw_size)
-
-
 class MainApp(MDApp):
     def __init__(self, sauce_path: str = 'data/sauce.json'):
         super().__init__()
-        self.label = MDLabel(text="Aboba", halign="center")
+        self.label = MDLabel(text="Aboba",
+                             halign='center',
+                             pos_hint={"center_x": 0.5, "center_y": 0.7},
+                             font_style='H4')
 
         with open(sauce_path, 'r') as f:
             sauce = json.load(f)
@@ -52,18 +32,18 @@ class MainApp(MDApp):
         return(
             MDScreen(
                 self.label,
-                BigAssButton(
-                    icon='circle',
-                    type='large',
+                MDIconButton(
+                    icon='circle-outline',
+                    icon_size=200,
+                    md_bg_color='#e9dff7',
                     pos_hint={"center_x": 0.5, "center_y": 0.35},
                     on_press=self.generate_callback
-                ),
+                )
                 # Reset button
-                MDIconButton(
-                    icon="restore",
-                    #type='standart',
-                    pos_hint={"center_x": 0.7, "center_y": 0.5},
-                ),
+                #MDIconButton(
+                #    icon="restore",
+                #    pos_hint={"center_x": 0.7, "center_y": 0.5},
+                #),
             )
         )
 
